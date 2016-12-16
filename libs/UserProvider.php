8875ae71d64e33682ddd3524375277d2a6a2b51b
Fenz <uukooo@126.com>
2016-12-16 16:59:45 +0800

    Add the Main file

---
+++ libs/UserProvider.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace CatPKT\UserProvider;
+
+use FenzHTTP\HTTP;
+
+////////////////////////////////////////////////////////////////
+
+class UserProvider
+{
+
+	/**
+	 * Var uri
+	 *
+	 * @access private
+	 *
+	 * @var    string
+	 */
+	private $uri;
+
+	/**
+	 * Var kei
+	 *
+	 * @access private
+	 *
+	 * @var    string
+	 */
+	private $key;
+
+	/**
+	 * Var method
+	 *
+	 * @access private
+	 *
+	 * @var    string
+	 */
+	private $method;
+
+	/**
+	 * Method __construct
+	 *
+	 * @access public
+	 *
+	 * @param  string $uri
+	 * @param  string $key
+	 * @param  string $method
+	 */
+	public function __construct( string$uri,string$key, string$method='AES-256-CBC' )
+	{
+		$this->uri= $uri;
+		$this->key= $key;
+		$this->method= $method;
+	}
+
+	/**
+	 * Method getToken
+	 *
+	 * @access public
+	 *
+	 * @param  string $thirdId
+	 *
+	 * @return string
+	 */
+	public function getToken( string$thirdId ):string
+	{
+		HTTP::url($this->uri)->post($this->encrypt([ 'thirdId'=>$thirdId, ]));
+	}
+
+	/**
+	 * Method encrypt
+	 *
+	 * @access private
+	 *
+	 * @param mixed $data
+	 *
+	 * @return void
+	 */
+	private function encrypt( $data ):void
+	{
+		$iv= random_bytes($this->ivLength);
+
+		$value= openssl_decrypt( serialize($data), $this->method, $this->key, 0, $iv );
+
+		$iv= base64_encode($iv);
+
+		$mac= hash_hmac( 'sha256', $iv.$value, $this->key );
+
+		return base64_encode(json_encode([ 'iv'=>$iv, 'value'=>$value, 'mac'=>$mac, ]));
+	}
+
+}
